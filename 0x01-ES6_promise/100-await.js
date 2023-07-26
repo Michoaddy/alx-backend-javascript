675c12f2b49200b3ca185aabcca1bfebe6f50879
michoaddy <michoaddy@gmail.com>
2023-07-26 23:31:57 +0000

    quick-fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,24 +3,18 @@
 
 // Write an async function named asyncUploadUser that will call these 
 // two functions and return an object with the following format:
-import { uploadPhoto, createUser } from "./utils.js";
 
-async function asyncUploadUser() {
-    try {
-      const photoPromise = uploadPhoto();
-      const userPromise = createUser();
-  
-      const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
-  
-      return {
-        photo: photoResponse,
-        user: userResponse,
-      };
-    } catch (error) {
-      console.error('Error:', error.message);
-      throw error;
-    }
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let res = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
   }
-  
-
-export {asyncUploadUser};
+  return res;
+}
