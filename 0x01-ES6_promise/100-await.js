eb2cbb65e31c12b7e47bcad9b117cc53a8063e34
michoaddy <michoaddy@gmail.com>
2023-07-26 23:35:21 +0000

    hot-fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,3 @@
-// Await / Async
-// Import uploadPhoto and createUser from utils.js
-
-// Write an async function named asyncUploadUser that will call these 
-// two functions and return an object with the following format:
-
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
